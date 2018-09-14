d57731780edfbceeb2c52bae2e2236b14ef0e22f
rahul <rahul@LAPTOP-2OQLS66J>
2018-09-15 05:54:55 +0700

    Initial Commit

---
+++ src/DynamicPrograming/LatihanDynamicPrograming.java
@@ -0,0 +1,16 @@
+package DynamicPrograming;
+import java.math.BigInteger;
+import java.util.Scanner;
+public class LatihanDynamicPrograming {
+    private static void tampilJudul(String identitas) {
+        System.out.println("Identitas : "+ identitas);
+        
+        System.out.println("\nHitung Fibonacci");
+        System.out.println("1, 1, 2, 3, 5, 8, 13, 21\n");
+    }
+    public static void main(String[] args) {
+        String identitas = "Rahul Fahmi Satria / X RPL 2 / 28";
+        tampilJudul(identitas);
+    
+    }
+}
